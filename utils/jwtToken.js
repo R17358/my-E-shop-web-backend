bd92c170121ee0b235a409764ff6b973fd8ff33a
Ritesh <shivay.lfc@gmail.com>
2024-12-20 15:02:54 +0530

    cookie

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,22 +1,25 @@
-// Create Token and saving in cookie
 const jwt = require("jsonwebtoken");
 
 const sendToken = (user, statusCode, res) => {
-  //const token = user.getJWTToken();
+  // Generate JWT Token
   const token = jwt.sign({ id: user._id }, "ABCD");
-  // options for cookie
+
+  // Options for cookie
   const options = {
-    expires: new Date(
-      Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-    ),
-    httpOnly: true,
+    expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+    httpOnly: true, // Prevents JavaScript access to cookies
+    secure: process.env.NODE_ENV === "production", // Only send cookies over HTTPS in production
+    sameSite: "None", // Necessary for cross-site cookies (CORS)
   };
 
-  res.status(statusCode).cookie("token", token, options).json({
-    success: true,
-    user,
-    token,
-  });
+  // Send response with cookie and token
+  res.status(statusCode)
+    .cookie("token", token, options)  // Send the cookie
+    .json({
+      success: true,
+      user,
+      token,  // Send the token in the response body as well (useful for the frontend)
+    });
 };
 
 module.exports = sendToken;
